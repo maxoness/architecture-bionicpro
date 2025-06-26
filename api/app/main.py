094c6f68bb71e0601bb20c4bee9d9aa92f270cf9
maxoness <31696-maxoness@users.noreply.hub.mos.ru>
2025-06-27 00:58:46 +0300

    minor changes

--- api/app/main.py
+++ api/app/main.py
@@ -1,16 +1,33 @@
+import logging
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi_keycloak_middleware import setup_keycloak_middleware
+from fastapi_keycloak_middleware import AuthorizationMethod, KeycloakConfiguration, setup
 
 from config import settings
 from routes import api_router
-from security import keycloak_config, scope_mapper
 
 app = FastAPI(
     title=settings.APP_NAME,
 )
 
-setup_keycloak_middleware(
+keycloak_config = KeycloakConfiguration(
+    url=settings.KEYCLOAK_URL,
+    realm=settings.KEYCLOAK_REALM,
+    client_id=settings.KEYCLOAK_CLIENT_ID,
+    client_secret=settings.KEYCLOAK_CLIENT_SECRET,
+    authorization_method=AuthorizationMethod.CLAIM,
+    authorization_claim="realm_access"
+)
+
+async def scope_mapper(claim_auth: dict) -> list[str]:
+    permissions = []
+    try:
+        permissions = claim_auth["roles"]
+    except KeyError:
+        logging.warning("Unknown roles")
+    return permissions
+
+setup.setup_keycloak_middleware(
     app,
     keycloak_configuration=keycloak_config,
     scope_mapper=scope_mapper,
